6b0363f7fc5aaa5e4e3b1d1e4a7feb518a558728
TheShadow1001 <theshadowkiller1234@gmail.com>
2025-05-22 22:48:09 +0200

    Update CherryApp.py

--- CherryApp.py
+++ CherryApp.py
@@ -1,5 +1,22 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="CherryApp",
+    page_icon="🍒",
+    layout="wide"
+)
+
+hide_everything = """
+    <style>
+    {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    </style>
+"""
+st.markdown(hide_everything, unsafe_allow_html=True)
+
+
+
 st.title("Bienvenido a CherryApp🍒")
 st.subheader("CherryCorporation🍒")
 st.image("https://dynamic.design.com/preview/logodraft/010ada47-fa29-4cfe-8c74-58ac48f80d6e/image/large.png")
@@ -11,26 +28,7 @@ st.write("Esta appWeb ayuda a la empresas a registrar los datos de un cliente a 
 st.write("La página web no es recomendable usarla como uso diario en una empresa, porque puede tener errores")
 st.write("pero dentro de muy poco tiempo la página ya no tendra errores a menudo.")
 
-st.title("Sobre nosotros🤔")
-st.image("https://imgs.search.brave.com/mIT3cPFH1knQmMFLf4IFRn_z_YpXqJZdlp6xJAiw670/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNjY5/ODg3MzUwL2VzL2Zv/dG8vZ2VudGUtZGUt/bmVnb2Npb3MtdHJh/YmFqYW5kby1lbi1l/c2NyaXRvcmlvLWRl/LXdpbmRvd3MuanBn/P3M9NjEyeDYxMiZ3/PTAmaz0yMCZjPVZi/a2VtS1JGWUJuY1df/SDd0ZEJIakw2eS1u/aUxuYXd0TzhzTTYt/U2Z5UzA9")
-st.text("Nosotros somos una corporación que se encarga de la producción de cosas que sean útiles para empresas")
-st.text("Siempre cualquier error con la página la resolverémos, pero la página puede presentar muchos de estos.")
-st.text("Pero cualquier errores puedes escribirnos por email:WikiDeveloper1@gmail.com")
-st.text("Esta página está en desarrollo, cualquier error critico puedes decirnos!")
-st.text("Si quieres ayudar en la página puedes contactarnos!")
-st.text("Y muchas gracias por visitar nuestro proyecto!")
-st.text("CherryCorporation es vida, CherryCorporation es amor.")
-st.title("")
-st.title("")
-st.title("Registrarme CherryApp🍒")
-st.image("https://dynamic.design.com/preview/logodraft/90dca979-d816-4e92-8cbd-e37b79f63f33/image/large.png")
-st.text("No es obligatorio registrarse!")
-st.text("Pero si te registras tendrás muchas ventajas en CherryApp🍒")
-st.text("¡Mucha suerte!")
-
-
 st.title("Registrarse")
-st.image("https://dynamic.design.com/preview/logodraft/90dca979-d816-4e92-8cbd-e37b79f63f33/image/large.png")
 st.write("Registrarse a CherryApp🍒, no es OBLIGATORIO, pero si quieres ser el primero es obtener las ")
 st.write("Actualizaciones de la página es recomendable registrarse, además podrás evitar fúturos pagos.")
 st.write("Ya que la página con el tiempo tendrá pagos, y sí creas una cuenta ahora podrás evitarlos a largo plazo.")
@@ -45,6 +43,32 @@ if st.button("Registrarme a CherryApp🍒"):
    else:
         st.info("Ok, gracias por su atención.")
 
+st.title("Sobre mí🤔")
+st.image("https://imgs.search.brave.com/mIT3cPFH1knQmMFLf4IFRn_z_YpXqJZdlp6xJAiw670/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNjY5/ODg3MzUwL2VzL2Zv/dG8vZ2VudGUtZGUt/bmVnb2Npb3MtdHJh/YmFqYW5kby1lbi1l/c2NyaXRvcmlvLWRl/LXdpbmRvd3MuanBn/P3M9NjEyeDYxMiZ3/PTAmaz0yMCZjPVZi/a2VtS1JGWUJuY1df/SDd0ZEJIakw2eS1u/aUxuYXd0TzhzTTYt/U2Z5UzA9")
+st.text("Soy un creador indie, de juego y de útilidades.")
+st.text("Siempre cualquier error con la página la resolveré, pero la página puede presentar muchos de estos")
+st.text("Pero cualquier errores puedes escribirme por email:WikiDeveloper1@gmail.com")
+st.text("Esta página está en desarrollo, cualquier error critico puedes decirnos!")
+st.text("Si quieres ayudar en la página puedes contactarme!")
+st.text("Y muchas gracias por visitar mí proyecto!")
+st.text("CherryApp es vida, CherryApp es amor")
+st.title("")
+st.title("")
+st.title("Registrarme CherryApp🍒")
+st.image("https://dynamic.design.com/preview/logodraft/90dca979-d816-4e92-8cbd-e37b79f63f33/image/large.png")
+st.text("No es obligatorio registrarse!")
+st.text("Pero si te registras tendrás muchas ventajas en CherryApp🍒")
+st.text("¡Cómo ser el primero en probar las actualizaciones!")
+
+Usuario = st.text_input("Usuario")
+Nombre = st.text_input("Nombre de empresa")
+cédula = st.text_input("Cédula")
+
+if st.button("Registrarme a CherryApp🍒", key="boton1"):
+    if Usuario and Nombre and cédula:
+        st.success("Registrado con éxito a CherryApp🍒")
+    else:
+        st.info("Gracias por su tiempo.")
         
 st.sidebar.title("Registros del cliente")
 st.sidebar.subheader("Aquí sería el apartado de registro")
@@ -60,5 +84,7 @@ if st.sidebar.button("Crear registro", key="boton2"):
         st.sidebar.warning("¡Es obligatorio poner un registro del cliente!")
 
 st.title("")
+
+
 
 st.text("Copyright© 2025 CherryApp.streamlit.app  |  All Rights Reserved  ")
