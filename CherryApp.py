f40446fb5c8a61484a28b75d24685f7640a05bf0
TheShadow1001 <theshadowkiller1234@gmail.com>
2025-05-22 20:59:52 +0200

    Update CherryApp.py

--- CherryApp.py
+++ CherryApp.py
@@ -11,21 +11,6 @@ st.write("Esta appWeb ayuda a la empresas a registrar los datos de un cliente a 
 st.write("La página web no es recomendable usarla como uso diario en una empresa, porque puede tener errores")
 st.write("pero dentro de muy poco tiempo la página ya no tendra errores a menudo.")
 
-st.title("Registrarse")
-st.write("Registrarse a CherryApp🍒, no es OBLIGATORIO, pero si quieres ser el primero es obtener las ")
-st.write("Actualizaciones de la página es recomendable registrarse, además podrás evitar fúturos pagos.")
-st.write("Ya que la página con el tiempo tendrá pagos, y sí creas una cuenta ahora podrás evitarlos a largo plazo.")
-
-nombre = st.text_input("Ingrese su usuario")
-correo = st.text_input("Ingrese su correo electrónico")
-empresa = st.text_input("Ingrese el nombre de su empresa")
-
-if st.button("Registrarme a CherryApp🍒"):
-   if nombre and correo and empresa:
-        st.success("¡Gracias por registrarse a CherryApp🍒!")
-   else:
-        st.info("Ok, gracias por su atención.")
-
 st.title("Sobre nosotros🤔")
 st.image("https://imgs.search.brave.com/mIT3cPFH1knQmMFLf4IFRn_z_YpXqJZdlp6xJAiw670/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNjY5/ODg3MzUwL2VzL2Zv/dG8vZ2VudGUtZGUt/bmVnb2Npb3MtdHJh/YmFqYW5kby1lbi1l/c2NyaXRvcmlvLWRl/LXdpbmRvd3MuanBn/P3M9NjEyeDYxMiZ3/PTAmaz0yMCZjPVZi/a2VtS1JGWUJuY1df/SDd0ZEJIakw2eS1u/aUxuYXd0TzhzTTYt/U2Z5UzA9")
 st.text("Nosotros somos una corporación que se encarga de la producción de cosas que sean útiles para empresas")
@@ -43,15 +28,23 @@ st.text("No es obligatorio registrarse!")
 st.text("Pero si te registras tendrás muchas ventajas en CherryApp🍒")
 st.text("¡Mucha suerte!")
 
-Usuario = st.text_input("Usuario")
-Nombre = st.text_input("Nombre de empresa")
-cédula = st.text_input("Cédula")
 
-if st.button("Registrarme a CherryApp🍒", key="boton1"):
-    if Usuario and Nombre and cédula:
-        st.success("Registrado con éxito a CherryApp🍒")
-    else:
-        st.info("Gracias por su tiempo.")
+st.title("Registrarse")
+st.image("https://dynamic.design.com/preview/logodraft/90dca979-d816-4e92-8cbd-e37b79f63f33/image/large.png")
+st.write("Registrarse a CherryApp🍒, no es OBLIGATORIO, pero si quieres ser el primero es obtener las ")
+st.write("Actualizaciones de la página es recomendable registrarse, además podrás evitar fúturos pagos.")
+st.write("Ya que la página con el tiempo tendrá pagos, y sí creas una cuenta ahora podrás evitarlos a largo plazo.")
+
+nombre = st.text_input("Ingrese su usuario")
+correo = st.text_input("Ingrese su correo electrónico")
+empresa = st.text_input("Ingrese el nombre de su empresa")
+
+if st.button("Registrarme a CherryApp🍒"):
+   if nombre and correo and empresa:
+        st.success("¡Gracias por registrarse a CherryApp🍒!")
+   else:
+        st.info("Ok, gracias por su atención.")
+
         
 st.sidebar.title("Registros del cliente")
 st.sidebar.subheader("Aquí sería el apartado de registro")
