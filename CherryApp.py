7fa7cc0da59dafdad3c9446126bbdec836122476
TheShadow1001 <theshadowkiller1234@gmail.com>
2025-05-31 18:12:46 +0200

    Update CherryApp.py

--- CherryApp.py
+++ CherryApp.py
@@ -2,13 +2,13 @@ import streamlit as st
 import webbrowser
 
 st.set_page_config(
-    page_title="CoinGames",
+    page_title="COINGAMES",
     page_icon="🧿",
     layout="wide",
     initial_sidebar_state="collapsed"
   )
 
-st.image("https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjUXlK85A0s4LWZMi9Bw6VjihteEbJW77sj6QjvVyPP14XKsGd8rLXoCSc_lglDEJXlUlVhF5kxPktYls73PEGRqT0E4_wJL82r8-tAQC5qeor6x898OLXcB3vHE60k3ha6c6DYLK_bBCv3jpNSRFpdi_LWrg2gtN19sBkcRpGPoLBbQg7uvNeoTH_dlwY/s600/wmremove-transformed%20(1).png")
+st.title("C O I N  🧿  G A M E S")
 
 hide_everything = """
     <style>
@@ -19,7 +19,7 @@ hide_everything = """
 """
 st.markdown(hide_everything, unsafe_allow_html=True)
 
-opcion = st.sidebar.radio("Elige una opcion", ["Juegos", "Archivos-Bat", "Apps-Útiles"])
+opcion = st.sidebar.radio("Elige una opcion", ["Juegos", "Celular", "Archivos-Bat", "Apps-Útiles"])
 
 st.markdown(f"## {opcion}")
 
@@ -63,7 +63,71 @@ if opcion == "Juegos":
     with col1:
         st.write("Grand Theft Auto 3")
         st.image("https://imgs.search.brave.com/1O50-6gOkWcsTwXH3rKRo7ZJvsiiC9cTWuRBiTCKt64/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJjYXZlLmNv/bS93cC93cDk4MzE5/MjMuanBn")
-        gta3 = st.button("Descagargar Grand Theft Auto 3")
+        gta3 = st.button("Descargar Grand Theft Auto 3")
     
         if gta3:
             webbrowser.open("https://download1648.mediafire.com/pc4odijgtfegTwl5cFON27kKryMW-fUIzGzSd4FQ0tgNwEa0ZoK5nVOvk9WsHO_Gz__gXptWp_KXZgwGsKRJKmJjqTWxIr20FOx_ItZpFIfInUtMu7CqtX6NpTOSJASUZVxKfaWmHV52LUNE_fKCJUHM4PyhtzoIJhrNzaOMzZXXYw/br2of7ckbp4sh4m/Gta3_Portable_By_TheShadow.rar")
+    with col3:
+        st.write("Counter Strike 1.6(1.0)")
+        st.image("https://imgs.search.brave.com/2F8vIUDyvQ4gH_3bJqMKdOVLB7-qjjZhxA5iPkA_hGQ/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJhY2Nlc3Mu/Y29tL2Z1bGwvMzUy/MTMwMy5qcGc")
+        csgo = st.button("Descargar Counter Strike(Source)")
+        
+        if csgo:
+            webbrowser.open("https://down-cs.su/download/1/direct")
+    with col1:
+        st.write("SchoolBoy Runaway_v1.0")
+        st.image("https://imgs.search.brave.com/GpVMKIuHrCs7WEcP-WfGxBtObNHKY4wd0g7VTu3bDtQ/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YXByb3h5LnR2dHJv/cGVzLm9yZy93aWR0/aC8xMjAwL2h0dHBz/Oi8vc3RhdGljLnR2/dHJvcGVzLm9yZy9w/bXdpa2kvcHViL2lt/YWdlcy9zY2hvb2xi/b3lydW5hd2F5LnBu/Zw")
+        school1 = st.button("Descargar SchoolBoy RunAway por Mediafire")
+        school2 = st.button("Descargar SchoolBoy RunAway por Gofile(No es directo)")
+        
+        if school1:
+            webbrowser.open("https://download1324.mediafire.com/4fke0tfnunugvZLcUPm5nEIOvSachvwvbzrmEWNkwz9PRAzFZqF7ViMxOkYSS-iEOBUec4EGqeyTPxCZL_caFE0Yv8-rYQY6meVuhSq8pX7o3de2Mhenw_pbssntYPLaBdgCkGJZp-QdmwiWPKmocLdoSb1QKVJjjnjvDTsRtioRMA/5b94f8ixd3j8yf0/SchoolBoy.Runaway+CoinGames.7z")
+            
+        if school2:
+            webbrowser.open("https://gofile.io/d/cTlw04")
+            
+    with col2:
+        st.write("Simpsons Hit And Run")
+        st.image("https://imgs.search.brave.com/vdsKIFGbaRNBmdWqPcbEGQYVYfMdCsMQqovqTy6GDbU/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9hcmNo/aXZlLm9yZy9kb3du/bG9hZC90aGUtc2lt/cHNvbnMtaGl0LXJ1/bl8yMDIyMDIvVGhl/X1NpbXBzb25zX0hp/dF9hbmRfUnVuX2Nv/dmVyLnBuZw")
+        hit = st.button("Descargar Simpsons Hit And Run")
+        
+        if hit:
+            webbrowser.open("https://download1351.mediafire.com/yorbdkbkndfgbgUM_q0Puu0bKgRnS-QrI83EYFOgP8LqEvUVRyYgdxHVfPxmYpRyZH8PX2wIZmYkxTGq8OhTnK9uIJlMHrcCD5vi4gY2Eyn5AoyQUbQs3btNbQV6qLx0ToliiZQzq_Gtkb5Wcy8114PM5MiYPtSS4MR7LCu8zj3MZw/0z9zevnxr0rgri3/TheSimpsons_Portable_By_TheShadow.rar")
+            st.info("'¡¡Gracias por descargar El Juego!!")
+
+if opcion == "Archivos-Bat":
+    st.subheader("Optimizadores, Atajos Etc")
+    optimizador = st.button("Descargar Optimizador(NORMAL)")
+    
+    if optimizador:
+        webbrowser.open("https://download1501.mediafire.com/vtbskqqc6eagCwP4UENghncfvE4m2ksy6thOerPGCHnE2KkRxfzybXXm1ScTlu6D2CA_4Be5NQgyMVPr6uV5HjOTE72lH7GwGALMO9TBhWWm2jlHfaI6py7ujQ6jwgzkLHzFv35usRYJcWucHa7nOMzSkiPbAKG5Zrrh8HkbvLBIrw/mfqp0juz05amllw/Optimizador_normal.7z")
+        
+if opcion == "Celular":
+    st.title("Programas-Juegos-Archivos")
+    st.write("Geometry Dash APK(BETA)")
+    st.image("https://imgs.search.brave.com/w9QiRfnv1cLFMLmuVjAkVZw6UUQGlsdb0V8bU0-mcMg/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMtd2l4bXAtZWQz/MGE4NmI4YzRjYTg4/Nzc3MzU5NGMyLndp/eG1wLmNvbS9mLzQ0/YmM2ZDM0LTQyOTIt/NDk1Ny05YTg0LTFj/OThjNzc0MGQ4NS9k/aGZqNDhuLWJhNGE5/MGE4LTg4OTQtNGU4/Ny05ZTM5LWFmNWFm/NTRhY2MzMS5wbmc_/dG9rZW49ZXlKMGVY/QWlPaUpLVjFRaUxD/SmhiR2NpT2lKSVV6/STFOaUo5LmV5Snpk/V0lpT2lKMWNtNDZZ/WEJ3T2pkbE1HUXhP/RGc1T0RJeU5qUXpO/ek5oTldZd1pEUXhO/V1ZoTUdReU5tVXdJ/aXdpYVhOeklqb2lk/WEp1T21Gd2NEbzNa/VEJrTVRnNE9UZ3lN/alkwTXpjellUVm1N/R1EwTVRWbFlUQmtN/alpsTUNJc0ltOWlh/aUk2VzF0N0luQmhk/R2dpT2lKY0wyWmNM/elEwWW1NMlpETTBM/VFF5T1RJdE5EazFO/eTA1WVRnMExURmpP/VGhqTnpjME1HUTRO/Vnd2WkdobWFqUTRi/aTFpWVRSaE9UQmhP/QzA0T0RrMExUUmxP/RGN0T1dVek9TMWha/alZoWmpVMFlXTmpN/ekV1Y0c1bkluMWRY/U3dpWVhWa0lqcGJJ/blZ5YmpwelpYSjJh/V05sT21acGJHVXVa/RzkzYm14dllXUWlY/WDAudnQwWURwdnF3/RlVYSWVBRzN0bkFx/MkhQVHExVFBablVs/WEZPWTh3djgxbw")
+    geometry = st.button("Descargar Geometry Dash")
+    
+    if geometry:
+        webbrowser.open("https://download1085.mediafire.com/jhyaytm72ifgVRXVD4X0dzh2mWrEWL_RyVKmJ1Xa-P8oDLptwzbW1eOfB0XhuQSRvU3bf3oFXU2IAGN4S-g1anKAKLNa5wAfkEKIF3JTfPiAzRyM6TLHZW_IV6tkXUyACVFtwNKamEEW34wS0_eGVpEzRSeqBvx2BNA2kzRwsp7Z1A/7xhqnxjjlmnwu25/GeometryDash_By_%40WikiDev.apk")
+        st.info("¡¡Gracias por descargar El Juego!!")
+
+if opcion == "Apps-Útiles":
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.write("Discord Lite(Combatible con windows 7)")
+        st.image("https://lh5.googleusercontent.com/ZggWfvCANuFBWzS8B1Wi80XyjDY98XmwBzRjvw2zT3Eqk0qLO50H68T7KCXSYG-F7QV2kMy2RmrBbKNu5QdU9gOxyTbesBJEMiBrXNBrBMybx8qMxDO7AjdY4dlO36-2BQ=w1280")
+        discord = st.button("Descargar Discord Lite")
+    
+        if discord:
+            webbrowser.open("https://download1589.mediafire.com/7kwi9b60zukglViFqn_Q_ngtKpyQXtJ0El8cO6VvvV6qq3zNilAxy1CzocZoddZmxEmijgT8LObjIIY2OPdxK1uIOOxzYUNizfX4EUfBE0JlKKD_Rbl8PAQ-2wbYNhf9q73Nss-0ujKS4YAVGGVCEumBK1e0ePuo_eskvR-HFVR5Xw/u7rcwpnlpc8cgcm/Discord_by_WikiDev.7z")
+            st.info("¡¡Gracias por descargar el programa!!")
+        
+    with col2:
+        st.write("Mem reduct")
+        st.image("https://lh6.googleusercontent.com/hu2q8PUiWR0a25TiZ7qnRj3qrAHSZWjymP8UN5P7cDGm8GeCvOYeucfB_L2yWwXv4hhKTJ4eLdONTreuZXwbEFY2FAhdNQoT2WNsNjvInDlFY6RfJvt5mwj3TqkpDjSAfA=w1280")
+        mem = st.button("Descargar mem reduct")
+        
+        if mem:
+            webbrowser.open("https://download946.mediafire.com/d2vy983bplsgtXaZ2kDDzF_rGE1lLIf7pavsMRZVVVFWir1AckzWD-8D3iRxuPnzlKxjSoV9wwlcMrbQ5YDClRV4u11vVQxmSQoa6DhRb3AWuBcnavg79MHzC_ddFSwad8imFSm1gp1ZBKlPIjbwKUVryWmIFuErWD-WhZNR4XyyYQ/3qdw5503pdzo93p/MemReduct_By_WikiDev.7z")
+            st.info("¡¡Gracias por descargar el programa!!")
